dd14d40227008ddc87f4f5d1d0ed1a7ba07c8a3b
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-10-13 20:48:43 -0300

    Next Lesson: Criando categoria

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Next Lesson: Debugando a aplicação"
+git commit -m "Next Lesson: Criando categoria"
 git push
 git status
--- projects/typescript/src/server.ts
+++ projects/typescript/src/server.ts
@@ -3,8 +3,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
     return response.json({ message: "Project Started" })
+});
+
+app.post("/logCourses", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ nameOfCourse: name })
 });
 
 app.listen(3030, () => console.log("Server is Up!"));
