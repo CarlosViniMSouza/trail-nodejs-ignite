82fd29e2ebca70b18be6f6c633e4f2356373a73d
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-11-14 20:07:09 -0300

    Now: Refatorando o model de categoria

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Now: Criando migration de categoria"
+git commit -m "Now: Refatorando o model de categoria"
 git push
 git status
---
+++ projects/typescript/src/database/migrations/1668465927080-CreateMigrationsCategories.ts
@@ -0,0 +1,38 @@
+import { table } from "console";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMigrationsCategories1668465927080 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "categories",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("categories");
+    }
+
+}
